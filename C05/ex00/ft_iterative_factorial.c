0aa98d0c9af0c62d1e0fa27e691d8bd80e257e1b
vboxuser <izakukawska1600@gmail.com>
2024-10-16 13:08:20 +0000

    C05 math exercises

---
+++ C05/ex00/ft_iterative_factorial.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int	ft_iterative_factorial(int nb)
+{
+	int	result;
+
+	result = 1;
+	if (nb < 0)
+	{
+		return (0);
+	}
+	while (nb > 0)
+	{
+		result *= nb;
+		nb--;
+	}
+	return (result);
+}
+
+int	main(void)
+{
+	printf("%d", ft_iterative_factorial(5));
+	return (0);
+}
